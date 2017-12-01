544cc9df58d49779d4ce166538da0a8425ec23d5
Matt Hardman <matthardman@Matts-MacBook-Pro.local>
2017-12-01 16:58:42 +0000

    adding all dynamic content to how

--- public/js/index.js
+++ public/js/index.js
@@ -3,25 +3,38 @@ $('#welcome_one').delay(1000).fadeIn();
 $('#welcome_two').delay(2000).fadeIn();
 $('.buttons').delay(3000).fadeIn();
 
+
+
 let scroller = function () {
 	 $('html, body').animate({scrollTop:5000},1000);
 }
 
-const key = 'ddeff3a422bf4bb0bf7b4d3e630e448c'
+//location
+$.getJSON('//freegeoip.net/json/?callback=?', function(data) {
+  console.log(data);
+  $('#how_9').html(`<div>From ${data.city}, ${data.region_name} to the North Pole is ${data.latitude * 69} miles.</div>` + '<br' +
+  	`<div class="row"><center><img src="/img/north-pole.jpg" width="250px"/></center></div>`)
+});
 
-var url = 'https://newsapi.org/v2/top-headlines?' +
+
+//news stories
+
+const keyNews = 'ddeff3a422bf4bb0bf7b4d3e630e448c'
+
+let urlNews = 'https://newsapi.org/v2/top-headlines?' +
           'sources=bbc-news&' +
-          `apiKey=${key}`;
+          `apiKey=${keyNews}`;
 
+let num = Math.floor((Math.random() ) * 5)
 
-var num = Math.floor((Math.random() )* 3)
-
-$.get( url, function( data ) {
+$.get( urlNews, function( data ) {
   $('#how_7').html(`<div class="row"><center><strong>${data.articles[num].title}</strong></center></div>` + `<hr>` +
   	`<div class="row"><center><img src="${data.articles[num].urlToImage}" width="250px"/></center></div>` + `<br>` +
   	`<div class="row">${data.articles[num].description}</div>` + `<hr>` +
   	`<a href="${data.articles[num].url}"><p>${data.articles[num].url}</p></a>`);
 });
+
+//pictures
 
 switch (num) {
     case 0:
@@ -32,6 +45,12 @@ switch (num) {
         break;
     case 2:
         $('#how_5').html('<div class="row"><center><img src="/img/doggy.jpg" width="250px"/></center></div>');
+        break;
+    case 3:
+        $('#how_5').html('<div class="row"><center><img src="/img/shower-curtain.jpg" width="250px"/></center></div>');
+        break;
+    case 4:
+        $('#how_5').html('<div class="row"><center><img src="/img/bacon-bandages.jpg" width="250px"/></center></div>');
         break;
 }
 

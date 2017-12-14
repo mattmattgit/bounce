95895b5c89606f0dfdd05c8ed45befa03bf53fc8
Matt Hardman <matthardman@matts-mbp.home>
2017-12-14 13:01:23 +0000

    minor changes

--- public/js/info.js
+++ public/js/info.js
@@ -27,19 +27,26 @@ $('#email-form').on('submit', function (e) {
 socket.on('emailSuccess', function (message) {
 	console.log('new message', message);
 
-	$('#want_section').append(`<div class='gap'></div><div class='emailThanks'><p>Thanks ${message} ` + 
-	`<span class="emoji">😍</span>. We'll be in touch shortly.</p></div><br>` + 
-	`<div id="about_3" class="large"><p>If you haven't already why not check out some of our additional info 🤓</p></div>` +
-	`<div class="gap"></div>` +
-	`<div id="more_about" class="view_more"><center><p>More info +</i></p></center></div>` +
-	`<div class="gap"></div>`)
+	$('#want_section').append(
+	`<div class='gap'></div>
+	<div class='emailThanks'>
+		<p>Thanks ${message}
+		<span class="emoji">😍</span>.
+		</p>
+	</div>
+	<div class='gap'></div>
+	<div class='emailThanks'>
+		<p>We'll be in touch shortly.</p>
+	</div>
+	<br>`);
+
+	$('.buttons').delay(1000).fadeIn(scroller);
 
 	scroller()
 
 });
 
-
-
 socket.on('disconnect', function () {
 	console.log('broken');
 });
+
